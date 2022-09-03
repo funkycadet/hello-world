629afbf3c9a8d50e6af822ec4479341fae6c448a
smith2eric <ericsmithsonian2@gmail.com>
2022-09-03 22:10:05 +0100

    [update]: Updated app file and added index file

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+""" Simple web app for CI """
+import os
+from flask import Flask, current_app
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """main route to return index.html"""
+    return current_app.send_static_file("index.html")
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT"))
+    app.run(debug=True, host="0.0.0.0", port=port)
